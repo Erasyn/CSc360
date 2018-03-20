9ad8040840cec59afdc7a7ac9a4b176ecd2bc16e
Erasyn <wyll.brimacombe@gmail.com>
2018-03-19 23:22:14 -0700

    to do

--- p3/master.c
+++ p3/master.c
@@ -8,10 +8,19 @@
 #include <time.h>
 
 void diskinfo(int argc, char* argv[]) {
+    int ph = 1;
+    //super block
+    printf("Super block information:\nBlock size: %d\nBlock count: %d\nFAT starts: %d\n",ph,ph,ph);
+    printf("FAT blocks: %d\nRoot directory start: %d\nRoot directory blocks %d\n\n",ph,ph,ph);
+    // FAT info
+    printf("FAT information:\nFree Blocks: %d\nReserved Blocks: %d\nAllocated Blocks: %d\n",ph,ph,ph);
+    printf("%d\n",argv[1][0]);
     return;
 }
 
 void disklist(int argc, char* argv[]) {
+    //for loop
+    printf("c1 8d 20s 17s");
     return;
 }
 
@@ -39,3 +48,5 @@ int main(int argc, char* argv[])
 #endif
  return 0;
 }
+
+// https://stackoverflow.com/questions/321423/parsing-binary-data-in-c
